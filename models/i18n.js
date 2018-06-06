792c07266ed16127d15ec8308a35b6f66ee1e0a9
xiaoyu <616872074@qq.com>
2018-06-07 00:17:56 +0800

    start jade engin

--- models/i18n.js
+++ models/i18n.js
@@ -1,12 +1,14 @@
 import i18n from 'i18n';
+console.log(__dirname)
 
 i18n.configure({
   locales: ['zh-CN', 'en-US'],
   cookie: 'locale',
   queryParameter: 'locale',
-  defaultLocale: 'en-US',
+  defaultLocale: 'zh-CN',
   objectNotation: true,
   syncFiles: false,
+  directory: __dirname + '/../language' // 默认解析文件
 });
 
 export default i18n;
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 import { getConfig } from '../utils/tool';
 
 router.get('/', (req, res, next) => {
+  console.log(res.__)
   async.parallel([
     fn => {
       getConfig(path.join(process.cwd(), 'config/settings.json'), (err, settings) => {
