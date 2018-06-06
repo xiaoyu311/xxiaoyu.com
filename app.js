c56d7fe91ffec9811835a70b764d4247b049df21
xiaoyu <616872074@qq.com>
2018-06-06 19:02:42 +0800

    add layout.jade

---
+++ app.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import route from './routes';
+import i18n from './models/i18n';
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+// i18n init parses req for language headers, cookies, etc.
+app.use(i18n.init);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', route);
+
+const normalizePort = val => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = app.listen(port, err => {
+  if (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`服务正在${server.address().port}端口启动`);
+    }
+  }
+});
+
+
+export default app;
