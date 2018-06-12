12822827c467e57d8b35e8ce27f8acc97815dbc7
xiaoyu <616872074@qq.com>
2018-06-12 23:48:27 +0800

    base64

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,4 +11,37 @@ $(function () {
     $(this).fadeOut(300);
     $('.mask_content').removeClass('mask_content_show');
   });
+  $('#button').on('click', function() {
+    
+    //检验是否为图像文件
+    var file = document.getElementById("one").files[0];
+    var reader = new FileReader();
+    //将文件以Data URL形式读入页面
+    reader.readAsDataURL(file);
+    reader.onload = function (e) {
+      console.log(reader.result)
+      var formdata = new FormData()
+
+      formdata.append('imgData', reader.result)
+      console.log(formdata)
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', '/admin/uploadimg', true);
+      xhr.send(formdata);
+      // $.ajax({
+      //   type: 'post',
+      //   url: '/admin/uploadimg',
+      //   dataType: 'json',
+      //   data: formdata,
+      //   cache: false,
+      //     processData: false,
+      //   success: function(res) {
+      //     console.log(res);
+      //   },
+      //   error: function(err) {
+      //     console.log(err)
+      //   }
+      // })
+      //显示文件
+    }
+  })
 });
--- routes/admin.js
+++ routes/admin.js
@@ -31,7 +31,7 @@ router.post('/saveCategories', (req, res, next) => {
 router.get('/getCategory', (req, res, next) => {
   category.getAll('Alias', 'all', (err, CategoryList) => {
     if (err) {
-      console.log('查询所有分类',err);
+      console.log('查询所有分类', err);
     } else {
       res.send({
         status: true,
@@ -44,36 +44,51 @@ router.get('/getCategory', (req, res, next) => {
 
 // 上传图片
 router.post('/uploadimg', (req, res, err) => {
-  let AVATAR_UPLOAD_FOLDER = '/public/images/';
-  let domain = "http://localhost:3001";
+  // console.log(req.body);
+  // console.log(req.query)
+  // let AVATAR_UPLOAD_FOLDER = '/public/images/';
+  // let domain = "http://localhost:3001";
   const form = new formidable.IncomingForm();
-  form.encoding = 'utf-8';
-  form.uploadDir = path.join(__dirname, '../public/images/'); //文件上传 临时文件存放路径
-  form.keepExtensions = true;
-  form.maxFieldsSize = 3 * 1024 * 1024;
+  // form.encoding = 'utf-8';
+  // form.uploadDir = path.join(__dirname, '../public/images/'); //文件上传 临时文件存放路径
+  // form.keepExtensions = true;
+  // form.maxFieldsSize = 3 * 1024 * 1024;
   form.parse(req, (err, fields, files) => {
-    var extName = ''; //后缀名
-    switch (files.file.type) {
-      case 'image/pjpeg':
-        extName = 'jpg';
-        break;
-      case 'image/jpeg':
-        extName = 'jpg';
-        break;
-      case 'image/png':
-        extName = 'png';
-        break;
-      case 'image/x-png':
-        extName = 'png';
-        break;
-    }
-    let avatarName = Math.random() + '.' + extName;
-    let newPath = form.uploadDir + avatarName;
-    let showUrl = domain + AVATAR_UPLOAD_FOLDER + avatarName;
-    fs.renameSync(files.file.path, newPath); //重命名
-    res.send({
-      path: showUrl
-    })
+    const imgData = fields.imgData.replace(/^data:image\/\w+;base64,/, '');
+    console.log(imgData)
+  
+    const dataBuffer = new Buffer(imgData, 'base64');
+    fs.writeFile('img.jpg', dataBuffer, function (err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send({
+          one: 'one'
+        })
+      }
+    });
+    //   var extName = ''; //后缀名
+    //   switch (files.file.type) {
+    //     case 'image/pjpeg':
+    //       extName = 'jpg';
+    //       break;
+    //     case 'image/jpeg':
+    //       extName = 'jpg';
+    //       break;
+    //     case 'image/png':
+    //       extName = 'png';
+    //       break;
+    //     case 'image/x-png':
+    //       extName = 'png';
+    //       break;
+    // }
+    //   let avatarName = Math.random() + '.' + extName;
+    //   let newPath = form.uploadDir + avatarName;
+    //   let showUrl = domain + AVATAR_UPLOAD_FOLDER + avatarName;
+    //   fs.renameSync(files.file.path, newPath); //重命名
+    //   res.send({
+    //     path: showUrl
+    //   })
   })
 })
 
