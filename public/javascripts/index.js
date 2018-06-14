17ab515a60bd16e1b07b64e147f1c0cb4df02b7b
xiaoyu <616872074@qq.com>
2018-06-14 16:31:56 +0800

    add mask content

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,35 +5,54 @@ $(function () {
     var _id = $(this).attr('_id');
     var title = $(this).attr('title');
     var url = window.location.pathname;
+    var element = '';
     if (currentId != 'target_url') {
       $('.mask').fadeIn(300);
       $('.mask_content').addClass('mask_content_show');
     }
     $('.header_title').children().text(title);
-    setTimeout(function(){
+    setTimeout(function () {
       $.ajax({
         type: 'GET',
         url: url + '/' + _id,
         dataType: 'json',
-        success: function(res) {
+        success: function (res) {
           if (res.status) {
             $('.my_load').css('display', 'none');
             $('.detail_content').css('display', 'block');
+            for (var i = 0; i < res.data.Labels.length; i++) {
+              element += ('<span class="label_item">' + res.data.Labels[i] + '</span>')
+            }
+            if (element) {
+              $('.labels').append(element);
+              var footerHeight = $('#footer_btn').height();
+              console.log(footerHeight)
+              $('#article_content').css('bottom', footerHeight + 20 + 'px');
+            }
             $('.mask_content').children('main').append(
               '<div class="detail_content">' + res.data.Content + '</div>'
             );
           }
         },
-        error: function(err) {
+        error: function (err) {
           console.log(err);
         }
       });
     }, 500)
   });
   $('.mask').on('click', function () {
-    $(this).fadeOut(300);
+    maskHide();
+  });
+  $('.btn_closs').on('click', function() {
+    maskHide();
+  });
+  function maskHide() {
+    $('.mask').fadeOut(300);
     $('.mask_content').removeClass('mask_content_show');
     $('.my_load').css('display', 'flex');
     $('.detail_content').css('display', 'none');
-  });
+    $('#article_content').css('bottom', 0);
+    element = '';
+    $('.label_item').remove();
+  }
 });
