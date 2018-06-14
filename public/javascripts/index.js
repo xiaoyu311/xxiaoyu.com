5c108bd0925501dee6dec0947d1131df41e748cf
xiaoyu <616872074@qq.com>
2018-06-14 15:25:08 +0800

    add mask content

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,29 +3,37 @@ $(function () {
   $('._content_item').on('click', function (event) {
     var currentId = $(event.target).attr('id');
     var _id = $(this).attr('_id');
+    var title = $(this).attr('title');
     var url = window.location.pathname;
     if (currentId != 'target_url') {
       $('.mask').fadeIn(300);
       $('.mask_content').addClass('mask_content_show');
     }
-    console.log(window)
-    $.ajax({
-      type: 'GET',
-      url: url + '/' + _id,
-      dataType: 'json',
-      success: function(res) {
-        if (res.status) {
-          console.log(res.data)
-          // $('.mask_content').append(res.data.Content);
+    $('.header_title').children().text(title);
+    setTimeout(function(){
+      $.ajax({
+        type: 'GET',
+        url: url + '/' + _id,
+        dataType: 'json',
+        success: function(res) {
+          if (res.status) {
+            $('.my_load').css('display', 'none');
+            $('.detail_content').css('display', 'block');
+            $('.mask_content').children('main').append(
+              '<div class="detail_content">' + res.data.Content + '</div>'
+            );
+          }
+        },
+        error: function(err) {
+          console.log(err);
         }
-      },
-      error: function(err) {
-        console.log(err);
-      }
-    });
+      });
+    }, 500)
   });
   $('.mask').on('click', function () {
     $(this).fadeOut(300);
     $('.mask_content').removeClass('mask_content_show');
+    $('.my_load').css('display', 'flex');
+    $('.detail_content').css('display', 'none');
   });
 });
